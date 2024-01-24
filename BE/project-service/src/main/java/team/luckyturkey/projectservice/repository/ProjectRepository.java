4d2671e4b3bca4ff12e30263715806fcaf029ee9
dltjdgh69 <dltjdgh69@gmail.com>
2024-01-24 12:33:25 +0900

    [chore]: entity, mongodb connection

--- BE/project-service/src/main/java/team/luckyturkey/projectservice/repository/ProjectRepository.java
+++ BE/project-service/src/main/java/team/luckyturkey/projectservice/repository/ProjectRepository.java
@@ -1,4 +1,10 @@
 package team.luckyturkey.projectservice.repository;
 
-public interface ProjectRepository{
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import team.luckyturkey.projectservice.document.Project;
+
+@Repository
+public interface ProjectRepository extends MongoRepository<Project, Long> {
 }
