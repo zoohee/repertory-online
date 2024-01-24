5fd925cbe43b721c8d35b026cc8fe54aaeb9f5ad
김형진 <rlagudwls3469@gmail.com>
2024-01-24 15:38:42 +0900

    Feat : 공용 Input 컴포넌트 생성

--- FE/src/components/common/Button.tsx
+++ FE/src/components/common/Button.tsx
@@ -1,55 +1,65 @@
-import React from "react";
-import styled,{css} from "styled-components";
-import Symbol from "../../images/GoogleSymbol.png"
+import styled, { css } from 'styled-components';
+import Symbol from '../../images/GoogleSymbol.png';
+import { ButtonHTMLAttributes } from 'react';
 const buttonStyles = {
-    default: css`
-        background-color: #0D0D0D;
-        color : white;
-    `,
-    submit: css`
-        color : #0D0D0D;
-        background-color: #FEE800;
-        &:hover{
-            cursor: pointer;
-            background-color: #c2b200;
-        }
-    `,
-    google: css`
-        color : #0D0D0D;
-        background-color: #F0F0F0;
-        &:hover{
-            cursor: pointer;
-            background-color: #a8a8a8;
-        }
-    `
+  default: css`
+    background-color: #0d0d0d;
+    color: white;
+  `,
+  submit: css`
+    color: #0d0d0d;
+    background-color: #fee800;
+    &:hover {
+      cursor: pointer;
+      background-color: #c2b200;
+    }
+  `,
+  google: css`
+    color: #0d0d0d;
+    background-color: #f0f0f0;
+    &:hover {
+      cursor: pointer;
+      background-color: #a8a8a8;
+    }
+  `,
 };
 const ButtonIcon = styled.img`
-    margin-right: 8px;
-    height:1.5rem;
-    width : auto;
-`
-const StyledButton = styled.button`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #0D0D0D;
-    color : white;
-    border: none;
-    width : 368px;
-    height : 3rem;
-    border-radius: 1.5rem;
-    margin : 12px 0 12px 0;
-    font-size : 1rem;
-    ${props => buttonStyles[props.btntype || 'default']}
-`
-const Button= ({btntype,buttonText,onClickEvent,})=> {
-    return(
-        <StyledButton
-            btntype={btntype}
-            onClick={onClickEvent}>
-            {btntype==="google" && <ButtonIcon src={Symbol}alt="Google-Symbol"/>}
-            {buttonText}
-        </StyledButton>
-    )
+  margin-right: 8px;
+  height: 1.5rem;
+  width: auto;
+`;
+
+interface StyledButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  btntype?: 'default' | 'submit' | 'google';
 }
+interface ButtonProps extends StyledButtonProps {
+  buttonText: string;
+}
+const StyledButton = styled.button<StyledButtonProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #0d0d0d;
+  color: white;
+  border: none;
+  width: 368px;
+  height: 3rem;
+  border-radius: 1.5rem;
+  margin: 12px 0 12px 0;
+  font-size: 1rem;
+  ${(props) => buttonStyles[props.btntype || 'default']}
+`;
+const Button: React.FC<ButtonProps> = ({
+  btntype,
+  buttonText,
+  onClick,
+  ...props
+}) => {
+  return (
+    <StyledButton btntype={btntype} onClick={onClick} {...props}>
+      {btntype === 'google' && <ButtonIcon src={Symbol} alt='Google-Symbol' />}
+      {buttonText}
+    </StyledButton>
+  );
+};
 export default Button;
