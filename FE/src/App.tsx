d94c310b01052e5aaa45d8401770a41ddef4472f
김세진 <sejin1398@gmail.com>
2024-01-25 09:24:20 +0900

    Fix: router 수정

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '@/pages/Home';
+
+import Index from '@/pages/Index';
 import SidebarPage from '@/pages/SidebarPage';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/*' element={<Home />} />
-        <Route path='/projects' element={<SidebarPage />} />
-        <Route path='/sources' element={<SidebarPage />} />
+        <Route path="/" element={<Index />} />
+        <Route path="/*" element={<SidebarPage />} />
       </Routes>
     </Router>
   );
