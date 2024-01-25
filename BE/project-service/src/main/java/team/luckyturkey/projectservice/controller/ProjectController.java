f212e35868c0c7cb33e8ba6c2188920e487b739e
dltjdgh69 <dltjdgh69@gmail.com>
2024-01-25 17:45:29 +0900

    Refactor: unused annotation

--- BE/project-service/src/main/java/team/luckyturkey/projectservice/controller/ProjectController.java
+++ BE/project-service/src/main/java/team/luckyturkey/projectservice/controller/ProjectController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import team.luckyturkey.projectservice.controller.requestdto.ProjectUpdateRequestDto;
