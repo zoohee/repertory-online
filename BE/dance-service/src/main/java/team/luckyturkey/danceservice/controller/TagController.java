9bc0aa4d2fef71656bce6fa67e2f817239c024f5
dltjdgh69 <dltjdgh69@gmail.com>
2024-01-26 17:54:32 +0900

    Feat: tag crud

---
+++ BE/dance-service/src/main/java/team/luckyturkey/danceservice/controller/TagController.java
@@ -0,0 +1,60 @@
+package team.luckyturkey.danceservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import team.luckyturkey.danceservice.controller.requestdto.PostTagRequest;
+import team.luckyturkey.danceservice.entity.Tag;
+import team.luckyturkey.danceservice.service.TagService;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/tag")
+@RequiredArgsConstructor
+public class TagController {
+
+    private final TagService tagService;
+
+    /**
+     * todo: get user id from jwt
+     * */
+    private final Long TEST_USER_ID = 1L;
+
+    @PostMapping
+    public ResponseEntity<Long> postTag(
+            @RequestBody PostTagRequest postTagRequest
+    ){
+        /**
+         * todo: get user id from jwt
+         * */
+        Tag tag = Tag.builder()
+                    .userId(TEST_USER_ID)
+                    .tagName(postTagRequest.getTagName())
+                    .build();
+        Long tagId = tagService.saveTag(tag);
+        return ResponseEntity.ok(tagId);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Tag>> getTagList(
+
+    ){
+        /**
+         * this is only for dummy
+         * todo: get user id from jwt
+         * */
+        List<Tag> tagList = tagService.getTagList(TEST_USER_ID);
+        return ResponseEntity.ok(tagList);
+    }
+
+    @DeleteMapping("/{tagId}")
+    public ResponseEntity<Long> deleteTag(
+            @PathVariable Long tagId
+    ){
+        tagService.deleteTag(tagId);
+        return ResponseEntity.ok(tagId);
+    }
+
+}
