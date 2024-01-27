d7df1f63d86a0b9d2eec64e17d283107188a374b
nijesmik <bkybb@duck.com>
2024-01-27 16:14:42 +0900

    Fix: Wrapper 컴포넌트 typescript 적용

--- FE/src/components/Wrapper.tsx
+++ FE/src/components/Wrapper.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ margin: string }>`
   margin: ${(props) => props.margin};
   display: flex;
   justify-content: space-between;
