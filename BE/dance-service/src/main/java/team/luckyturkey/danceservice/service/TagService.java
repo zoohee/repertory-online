90f11c140d085e8e5e263dc70c3218e799b098df
dltjdgh69 <dltjdgh69@gmail.com>
2024-01-29 08:54:40 +0900

    Refactor: request dto refactor

--- BE/dance-service/src/main/java/team/luckyturkey/danceservice/service/TagService.java
+++ BE/dance-service/src/main/java/team/luckyturkey/danceservice/service/TagService.java
@@ -1,11 +1,12 @@
 package team.luckyturkey.danceservice.service;
 
+import team.luckyturkey.danceservice.controller.requestdto.PostTagRequest;
 import team.luckyturkey.danceservice.entity.Tag;
 
 import java.util.List;
 
 public interface TagService {
-    Long saveTag(Tag tag);
+    Long saveTag(PostTagRequest postTagRequest, Long userId);
 
     void deleteTag(Long tagId);
 
