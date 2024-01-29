e800e3802b23957e54fffc682a024e76e43e6615
dltjdgh69 <dltjdgh69@gmail.com>
2024-01-30 03:45:02 +0900

    Add: ToString

--- BE/dance-service/src/main/java/team/luckyturkey/danceservice/controller/requestdto/PostSourceRequest.java
+++ BE/dance-service/src/main/java/team/luckyturkey/danceservice/controller/requestdto/PostSourceRequest.java
@@ -1,14 +1,16 @@
 package team.luckyturkey.danceservice.controller.requestdto;
 
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 
+@ToString
 @Getter
 public class PostSourceRequest {
     private String sourceName;
 	private int sourceLength;
-	private List<String> tagList;
+	private List<String> tagNameList;
 	private String start;
 	private String end;
 }
