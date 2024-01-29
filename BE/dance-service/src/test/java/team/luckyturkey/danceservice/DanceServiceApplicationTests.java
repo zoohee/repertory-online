0a5a8100c4baca4679cc6e9a4d0cfc55d116d09f
dltjdgh69 <dltjdgh69@gmail.com>
2024-01-30 03:43:32 +0900

    Fix: eureka re-patched

--- BE/dance-service/src/test/java/team/luckyturkey/danceservice/DanceServiceApplicationTests.java
+++ BE/dance-service/src/test/java/team/luckyturkey/danceservice/DanceServiceApplicationTests.java
@@ -1,10 +1,23 @@
 package team.luckyturkey.danceservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import team.luckyturkey.danceservice.entity.Source;
+import team.luckyturkey.danceservice.entity.Tag;
+import team.luckyturkey.danceservice.repository.jpa.SourceTagRepository;
+import team.luckyturkey.danceservice.repository.jpa.TagRepository;
+
+import java.util.List;
 
 @SpringBootTest
 class DanceServiceApplicationTests {
+
+	@Autowired
+	SourceTagRepository sourceTagRepository;
+
+	@Autowired
+	TagRepository tagRepository;
 
 	@Test
 	void contextLoads() {
