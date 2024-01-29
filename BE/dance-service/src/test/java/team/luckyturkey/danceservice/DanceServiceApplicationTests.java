a2f9143faa2d8819fd9610d19c3420acbed8fad2
dltjdgh69 <dltjdgh69@gmail.com>
2024-01-30 03:44:29 +0900

    Fix: nothing

--- BE/dance-service/src/test/java/team/luckyturkey/danceservice/DanceServiceApplicationTests.java
+++ BE/dance-service/src/test/java/team/luckyturkey/danceservice/DanceServiceApplicationTests.java
@@ -3,8 +3,6 @@ package team.luckyturkey.danceservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import team.luckyturkey.danceservice.entity.Source;
-import team.luckyturkey.danceservice.entity.Tag;
 import team.luckyturkey.danceservice.repository.jpa.SourceTagRepository;
 import team.luckyturkey.danceservice.repository.jpa.TagRepository;
 
