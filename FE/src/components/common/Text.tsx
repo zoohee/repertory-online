91f5d907a4ff6183f0f319bf6095f7be05dc94d6
김세진 <sejin1398@gmail.com>
2024-01-30 10:01:18 +0900

    Refactor: Text 컴포넌트 수정

--- FE/src/components/common/Text.tsx
+++ FE/src/components/common/Text.tsx
@@ -2,10 +2,23 @@ import styled from 'styled-components';
 
 import { fontSize } from '@/styles/font';
 
-const Text = styled.div<{ size: string; color: string }>`
+export class TextStyle {
+  size: string;
+  color: string;
+
+  constructor(size: string, color: string) {
+    this.size = size;
+    this.color = color;
+  }
+}
+
+const Text = styled.div<TextStyle>`
   ${({ size }) => {
     if (size === 's') {
       return fontSize.s;
+    }
+    if (size === 'xl') {
+      return fontSize.xl;
     }
     return fontSize.m;
   }};
