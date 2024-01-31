cebc32933ed79eba08780bfcc4ce5cfe72fcbc15
dltjdgh69 <dltjdgh69@gmail.com>
2024-01-31 16:48:42 +0900

    Add: source-repertory mapper table

---
+++ BE/dance-service/src/main/java/team/luckyturkey/danceservice/domain/entity/id/SourceRepertoryPK.java
@@ -0,0 +1,18 @@
+package team.luckyturkey.danceservice.domain.entity.id;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+
+@ToString
+@Getter
+@Setter
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class SourceRepertoryPK implements Serializable {
+    private Long repertoryId;
+    private Long sourceId;
+}
