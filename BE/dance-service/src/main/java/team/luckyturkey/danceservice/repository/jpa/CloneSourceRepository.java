c1c547103dd7f2d3483523ec9232963db07ad8d0
dltjdgh69 <dltjdgh69@gmail.com>
2024-02-02 16:49:58 +0900

    Feat: clone get, patch api

--- BE/dance-service/src/main/java/team/luckyturkey/danceservice/repository/jpa/CloneSourceRepository.java
+++ BE/dance-service/src/main/java/team/luckyturkey/danceservice/repository/jpa/CloneSourceRepository.java
@@ -1,7 +1,13 @@
 package team.luckyturkey.danceservice.repository.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import team.luckyturkey.danceservice.domain.entity.CloneSource;
 
- public interface CloneSourceRepository extends JpaRepository<CloneSource, Long> {
+import java.util.List;
+
+public interface CloneSourceRepository extends JpaRepository<CloneSource, Long> {
+
+ @Query("select cs from CloneSource cs join fetch cs.cloneSourceDetail where cs.memberId = :memberId")
+ List<CloneSource> findByMemberId(Long memberId);
 }
