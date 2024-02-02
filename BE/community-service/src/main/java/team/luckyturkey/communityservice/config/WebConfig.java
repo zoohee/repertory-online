c63fba4f5bf8db14724594bdf83c5291ecbc6ec3
JUHEE LEE <48899055+zoohee@users.noreply.github.com>
2024-02-02 13:29:48 +0900

    Fix: WebConfig allowedOrigins

--- BE/community-service/src/main/java/team/luckyturkey/communityservice/config/WebConfig.java
+++ BE/community-service/src/main/java/team/luckyturkey/communityservice/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*") // 허용할 출처
                 .allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH") // 허용할 HTTP method
-                .allowCredentials(true) // 쿠키 인증 요청 허용
+//                .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
 }
