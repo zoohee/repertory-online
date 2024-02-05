7e8a443d567c1b922dfba78a9912a93a689cba72
김세진 <sejin1398@gmail.com>
2024-02-05 14:09:10 +0900

    Revert "Merge branch 'FE/apiTest' into 'develop'"
    This reverts merge request !33

--- FE/src/components/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Wrapper = styled.div<{ margin: string }>`
-  margin: ${(props) => props.margin};
-  display: flex;
-  justify-content: space-between;
-`;
-
-export default Wrapper;
