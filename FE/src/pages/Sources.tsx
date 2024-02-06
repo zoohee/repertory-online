1c528233b96dd9b9a27237b89ac1efc83ff1f8fe
김세진 <sejin1398@gmail.com>
2024-02-06 10:07:24 +0900

    Feat: 태그 선택 기능 구현

--- FE/src/pages/Sources.tsx
+++ FE/src/pages/Sources.tsx
@@ -1,9 +1,22 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+
+import SourcesContextProvider from '@/store/sources-context';
 
 import TabButtons, { Tab } from '@/components/common/Tab';
 import SearchBar from '@/components/SearchBar';
 import SourceList from '@/components/dance/DanceGridBox';
 import SourceItem from '@/components/dance/Source';
+import Wrapper from '@/components/Wrapper';
+import CreateButton from '@/components/dance/CreateButton';
+import SelectTagButton from '@/components/dance/SelectTagButton';
+import TagList from '@/components/dance/TagList';
+
+const Box = styled.div`
+  margin: 24px 0;
+  padding: 0 12px;
+  width: 100%;
+`;
 
 const TABS: Tab[] = [
   new Tab('My Sources', true),
@@ -38,17 +51,32 @@ const SourcesPage = () => {
     );
   };
 
+  const [isTagListOpen, setIsTagListOpen] = useState(false);
+
+  const handleOpenTag = () => {
+    setIsTagListOpen((prev) => !prev);
+  };
+
   return (
-    <>
+    <SourcesContextProvider>
       <TabButtons tabs={tabs} margin="48px 0 0" onClickTab={handleClickTab} />
-      <SearchBar></SearchBar>
+      <Box>
+        <Wrapper $margin="0">
+          <div style={{ display: 'flex' }}>
+            <SearchBar></SearchBar>
+            <SelectTagButton openTagList={handleOpenTag} />
+          </div>
+          <CreateButton />
+        </Wrapper>
+        {isTagListOpen && <TagList />}
+      </Box>
       {/* TODO: 프로젝트 생성 버튼 */}
       <SourceList column={4}>
         {DUMMY_LIST.map((item, idx) => {
           return <SourceItem key={idx} source={item} />;
         })}
       </SourceList>
-    </>
+    </SourcesContextProvider>
   );
 };
 
