68d22e6abcd8e2eb6add43d39d47288ef3bda6a8
안준현 <erik1201@naver.com>
2024-02-13 22:05:20 +0900

    Fix : Add logoutSuccessHandler, Delete redirect url

--- BE/member-service/src/main/java/team/luckyturkey/memberservice/auth/handler/CustomLogoutSuccessHandler.java
+++ BE/member-service/src/main/java/team/luckyturkey/memberservice/auth/handler/CustomLogoutSuccessHandler.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
