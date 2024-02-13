677fd015e05622ed2a932e81ca7e9b354cdaa673
JUHEE LEE <48899055+zoohee@users.noreply.github.com>
2024-02-13 22:00:21 +0900

    Feat: 커뮤니티 디테일 다운로드 여부 추가

--- BE/dance-service/src/main/java/team/luckyturkey/danceservice/repository/jpa/CloneSourceRepository.java
+++ BE/dance-service/src/main/java/team/luckyturkey/danceservice/repository/jpa/CloneSourceRepository.java
@@ -10,4 +10,8 @@ public interface CloneSourceRepository extends JpaRepository<CloneSource, Long> 
 
  @Query("select cs from CloneSource cs join fetch cs.cloneSourceDetail where cs.memberId = :memberId")
  List<CloneSource> findByMemberId(Long memberId);
+
+ @Query("select count(cs) from CloneSource cs where cs.source.id = :originId and cs.memberId = :memberId")
+ int countByOriginIdAndMemberId(Long originId, Long memberId);
+
 }
