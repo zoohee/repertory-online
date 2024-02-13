95ea15178b058778442a98cf474371dd61e97b55
안준현 <erik1201@naver.com>
2024-02-13 11:03:43 +0900

    Fix : cors

--- BE/member-service/src/main/java/team/luckyturkey/memberservice/auth/config/SecurityConfig.java
+++ BE/member-service/src/main/java/team/luckyturkey/memberservice/auth/config/SecurityConfig.java
@@ -109,7 +109,7 @@ public class SecurityConfig {
                 .logout((logout) -> logout
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) //로그아웃 요청 url
                         .addLogoutHandler(customLogoutHandler)
-                        .logoutSuccessUrl("/")); //로그아웃에 성공하면 메인페이지로 이동
+                        .logoutSuccessUrl("https://repertory.online")); //로그아웃에 성공하면 메인페이지로 이동
 
 
 
--- BE/member-service/src/main/java/team/luckyturkey/memberservice/auth/handler/LogoutSuccessHandler.java
+++
@@ -1,7 +0,0 @@
-package team.luckyturkey.memberservice.auth.handler;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class LogoutSuccessHandler {
-}
