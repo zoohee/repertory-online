9af5f3ca9d26fc7afff9194330790fef07483a90
안준현 <erik1201@naver.com>
2024-02-13 22:52:08 +0900

    ãFix : delete Oauth redirect

--- BE/member-service/src/main/java/team/luckyturkey/memberservice/auth/handler/MyAuthenticationSuccessHandler.java
+++ BE/member-service/src/main/java/team/luckyturkey/memberservice/auth/handler/MyAuthenticationSuccessHandler.java
@@ -56,31 +56,38 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
             GeneratedToken token = jwtUtil.generateToken(memberId, memberEmail, memberRole);
             log.info("jwtToken = {}", token);
             String accessToken = token.getAccessToken();
+            String refreshToken = token.getRefreshToken();
 
-            // accessToken을 쿼리스트링에 담는 url을 만들어준다.
-            String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:8080/member/loginsuccess")
-                    .queryParam("accessToken", accessToken)
-                    .build()
-                    .encode(StandardCharsets.UTF_8)
-                    .toUriString();
-            log.info("redirect 준비");
-            // 로그인 확인 페이지로 리다이렉트 시킨다.
-            getRedirectStrategy().sendRedirect(request, response, targetUrl);
+            response.addHeader("Authorization", "Bearer " + accessToken);
+            response.addHeader("Refresh", "Bearer " + refreshToken);
+
+
+            //리다이렉트 ㄴㄴ
+//            // accessToken을 쿼리스트링에 담는 url을 만들어준다.
+//            String targetUrl = UriComponentsBuilder.fromUriString("http://repertory.online")
+//                    .queryParam("accessToken", accessToken)
+//                    .build()
+//                    .encode(StandardCharsets.UTF_8)
+//                    .toUriString();
+//            log.info("redirect 준비");
+//            // 로그인 확인 페이지로 리다이렉트 시킨다.
+//            getRedirectStrategy().sendRedirect(request, response, targetUrl);
 
 
         } else {
+            throw new IllegalArgumentException("비정상적 소셜로그인");
             //지금은 로그인과 동시에 회원가입을 한다. 그래서 여기에 도달하지 않아.
             //만약 커스텀 회원가입 창이 존재한다면, 여기서 새로운 리다이렉트요청을 보내서 나오는 페이지에서 회원가입을 해야한다.
             //
             // 회원이 존재하지 않을경우, 서비스 제공자와 email을 쿼리스트링으로 전달하는 url을 만들어준다.
-            String targetUrl = UriComponentsBuilder.fromUriString("http://3.39.72.204/loginSuccess")
-                    .queryParam("email", (String) oAuth2User.getAttribute("email"))
-                    .queryParam("provider", provider)
-                    .build()
-                    .encode(StandardCharsets.UTF_8)
-                    .toUriString();
-            // 회원가입 페이지로 리다이렉트 시킨다.
-            getRedirectStrategy().sendRedirect(request, response, targetUrl);
+//            String targetUrl = UriComponentsBuilder.fromUriString("http://3.39.72.204/loginSuccess")
+//                    .queryParam("email", (String) oAuth2User.getAttribute("email"))
+//                    .queryParam("provider", provider)
+//                    .build()
+//                    .encode(StandardCharsets.UTF_8)
+//                    .toUriString();
+//            // 회원가입 페이지로 리다이렉트 시킨다.
+//            getRedirectStrategy().sendRedirect(request, response, targetUrl);
         }
     }
 }
