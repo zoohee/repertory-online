2738f91f9e838c2bcbe78f03e81188d307c73548
김세진 <sejin1398@gmail.com>
2024-02-16 12:18:16 +0900

    Fix: 충돌 해결

---
+++ FE/src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Login from '@/pages/Login';
+import SignUp from '@/pages/SignUp';
+import Index from '@/pages/Index';
+const Home = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
+    </>
+  );
+};
+
+export default Home;
+
+
