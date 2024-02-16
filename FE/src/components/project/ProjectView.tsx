82d79674ae5eaa01af2bd7dbae48b76ed677b81f
김형진 <rlagudwls3469@gmail.com>
2024-02-16 11:21:43 +0900

    Chore : gitignore

--- FE/src/components/project/ProjectView.tsx
+++ FE/src/components/project/ProjectView.tsx
@@ -27,25 +27,6 @@ const FlexWrapper = styled.div`
   display: flex;
   justify-content: space-between;
 `;
-// const ModalOverlay = styled.div`
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   width: 100%;
-//   height: 100%;
-//   background-color: rgba(0, 0, 0, 0.5);
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `;
-
-// const ModalBox = styled.div`
-//   background-color: white;
-//   padding: 20px;
-//   border-radius: 10px;
-//   width: 500px;
-//   max-width 100%;
-// `;
 
 const PleaseUploadFile = styled.div`
   margin-top: 20%;
