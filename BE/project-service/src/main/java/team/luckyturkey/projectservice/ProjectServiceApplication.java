91f5e31911a9f5b468f08604addf04c58a99fbec
JUHEE LEE <48899055+zoohee@users.noreply.github.com>
2024-02-16 10:27:01 +0900

    Fix: merge conflicts

--- BE/project-service/src/main/java/team/luckyturkey/projectservice/ProjectServiceApplication.java
+++ BE/project-service/src/main/java/team/luckyturkey/projectservice/ProjectServiceApplication.java
@@ -2,9 +2,10 @@ package team.luckyturkey.projectservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class ProjectServiceApplication {
 
     public static void main(String[] args) {
